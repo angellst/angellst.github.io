68a5ec88024d0dca40fbbf71320de2be28ad6ade
angellst <133069920+angellst@users.noreply.github.com>
2023-05-22 08:37:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".myswiper",{
+    slidesPreview: 1,
+    spaceBetween: 30,
+    grabCursor:true,
+    loop: true,
+    breakpoints: {
+        991:{
+            slidesPreview:2
+        }
+    }
+})
